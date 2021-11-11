fd32a60fdbbd681ec2316d2c88b1b5fb23e90175
ksrof <kevinsunercontacto@gmail.com>
2021-11-11 12:50:28 +0100

    specific country api request, shadow dom innerHTML with api data

---
+++ components/country.js
@@ -0,0 +1,29 @@
+const apiUrl = "https://restcountries.com/v3.1/name/bolivia";
+
+class Country extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    fetch(apiUrl)
+      .then((resp) => resp.json())
+      .then((country) => {
+        for (let i = 0; i < country.length; i++) {
+          this.shadow.innerHTML = `
+            <div>
+              <h1>${country[i].name.common}</h1>
+              <ul>
+                <p>Language: <b>${country[i].languages.spa}</b></p>
+                <p>Population: <b>${country[i].population}</b></p>
+                <p>Continent: <b>${country[i].continents}</b></p>
+              </ul>
+            </div>
+          `;
+        }
+      });
+  }
+}
+
+customElements.define("country-details", Country);
